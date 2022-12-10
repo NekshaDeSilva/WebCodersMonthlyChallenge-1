d6e9607f77ac6e278a0d9e6d2e5cdf0658368c31
Nv <102156620+NekshaDeSilva@users.noreply.github.com>
2022-12-10 21:26:20 +0530

    Create Bug.js

---
+++ Bug.js
@@ -0,0 +1,15 @@
+const numberOne = 1;
+const numberTwo = 1000;
+const answer = math.round(numberOne+numberTwo/1);
+if(answer <= 1000){
+  
+}else{
+
+}
+function answer1(){
+console.log("yes");
+}
+
+function answer2(){
+console.log("No");
+}
